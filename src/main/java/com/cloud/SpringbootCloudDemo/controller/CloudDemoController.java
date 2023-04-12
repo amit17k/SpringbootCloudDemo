8be50981e81fbf2edcfaca9615775852318cfe43
Amit Gupta <Amit.Gupta18@gds.ey.com>
2023-04-12 17:15:02 +0530

    commit

--- src/main/java/com/cloud/SpringbootCloudDemo/controller/CloudDemoController.java
+++ src/main/java/com/cloud/SpringbootCloudDemo/controller/CloudDemoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cloud.SpringbootCloudDemo.model.Employee;
-
+//Controller
 @RestController
 public class CloudDemoController {
 	
