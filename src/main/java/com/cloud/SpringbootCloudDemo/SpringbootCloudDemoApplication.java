7733cf5dec8dc4899ac3778bf537317211f0da04
Amit Gupta <Amit.Gupta18@gds.ey.com>
2023-04-12 16:07:50 +0530

    first commit

---
+++ src/main/java/com/cloud/SpringbootCloudDemo/SpringbootCloudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.SpringbootCloudDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCloudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCloudDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/cloud/SpringbootCloudDemo/controller/CloudDemoController.java
@@ -0,0 +1,24 @@
+package com.cloud.SpringbootCloudDemo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cloud.SpringbootCloudDemo.model.Employee;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class CloudDemoController {
+	
+ @GetMapping("/employeeDetails")
+ public ResponseEntity<Employee> getEmployeeDetails() {
+	 Employee employee = new Employee();
+	 employee.setDesignation("Manager");
+	 employee.setEmpId(12);
+	 employee.setName("John Smith");
+	 
+ return ResponseEntity.ok(employee);
+
+}
+}
